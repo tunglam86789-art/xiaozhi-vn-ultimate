1803a3642b78bfaa7ee22832be423eb83717eb81
TienHuyIoTLab <tienhuyiot@gmail.com>
2025-12-28 22:28:35 +0700

    Fix over stack memory application

--- main/application.cc
+++ main/application.cc
@@ -408,7 +408,7 @@ void Application::Start() {
     xTaskCreate([](void* arg) {
         ((Application*)arg)->MainEventLoop();
         vTaskDelete(NULL);
-    }, "main_event_loop", 1024 * 3, this, 3, &main_event_loop_task_handle_);
+    }, "main_event_loop", 1024 * 3 + 512, this, 3, &main_event_loop_task_handle_);
 
     /* Start the clock timer to update the status bar */
     esp_timer_start_periodic(clock_timer_handle_, 1000000);
@@ -701,7 +701,7 @@ void Application::MainEventLoop() {
                                 app->UpdateIdleDisplay();
                             }
                             vTaskDelete(NULL);
-                        }, "weather_task", 8192, this, 5, NULL);
+                        }, "weather_task", 1024 * 4, this, 5, NULL);
                     }
                 }
             }
