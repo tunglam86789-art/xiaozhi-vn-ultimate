e5824b462b919ae094ff434ede13524c61605794
TienHuyIoTLab <tienhuyiot@gmail.com>
2025-12-31 22:16:31 +0700

    Cập nhật chương trình hẹn giờ báo thức

--- main/features/alarm_clock/alarm_clock.h
+++ main/features/alarm_clock/alarm_clock.h
@@ -1,4 +1,6 @@
 #ifndef ALARM_CLOCK_H
 #define ALARM_CLOCK_H
 // This feature is currently not implemented. It will be added in future releases.
+
+// Tôi sẽ tích hợp thêm chương trình đồng hồ báo thức vào dự án trong các phiên bản sau.
 #endif
